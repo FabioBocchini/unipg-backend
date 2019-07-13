2f85e53b3873d8c08c2bd63a7a80bbb3ef919339
FabioBocchini <46478546+FabioBocchini@users.noreply.github.com>
2019-07-13 14:17:43 +0200

    first commit

---
+++ controller/corsoController.js
@@ -0,0 +1,34 @@
+const {Pool} = require('pg')
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Unipg',
+  password: 'postgres',
+})
+
+const getCorso = (request, response) => {
+  pool.query('SELECT * FROM corso', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+//utilizzato per ricevere i corsi di un certo prof
+const getCorsoFromProf = (request, response) => { 
+  const professore = (request.params.professore)
+
+  pool.query('SELECT id_corso, nome FROM corso WHERE professore = $1', [professore], 
+  (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+})}
+
+module.exports = {
+  getCorso,
+  getCorsoFromProf
+}
---
+++ controller/studenteController.js
@@ -0,0 +1,73 @@
+const {Pool} = require('pg')
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Unipg',
+  password: 'postgres',
+})
+
+//ritorna la lista degli studenti iscritti
+const getStudente = (request, response) => {
+  pool.query('SELECT matricola FROM studente', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+//controlla l'esistenza di una email o di una matricola durante la registrazione
+const checkEmail = (request, response) => {
+  const {email, matricola}=request.body
+  let value = false
+  pool.query('SELECT * FROM studente WHERE email = $1 OR matricola = $2',[email, matricola],
+  (error, results) =>  {
+    if (error) {
+      throw error
+    }
+    if(results.rows.length < 1){
+      return response.status(201).send(`Nuovo Studente: ${matricola}`)
+    }
+    response.sendStatus(401)
+  })
+}
+
+//usato per login studente
+const postStudLogin = (request, response) => { 
+  const {email,password}= request.body
+  pool.query('SELECT matricola, nome, cognome, password FROM studente WHERE email = $1', [email], 
+  (error, results) => {
+    if (error) {
+      throw error
+    }
+    if(results.rows.length > 0){
+      const user = results.rows[0]
+      if(user.password === password) {
+        delete user.password
+        return response.status(200).json(user)
+      }
+    }
+    response.sendStatus(401)
+  })
+}
+
+//usato per la registrazione di un nuovo studente
+const postNuovoStudente = (request, response) => { 
+  const {matricola,nome,cognome,email,password}= request.body
+  pool.query('INSERT INTO studente VALUES($1,$2,$3,$4,$5)', [matricola,nome,cognome,email,password], 
+  (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Studente added: ${matricola}`)
+  })
+}
+
+
+module.exports = {
+  getStudente,
+  postStudLogin,
+  postNuovoStudente,
+  checkEmail
+}
