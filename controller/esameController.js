9c5aa2bea89ab21ebb166e78fd82dae2269b32ec
unknown <iambiozzus@gmail.com>
2019-07-14 16:51:27 +0200

    Prettyfied

--- controller/esameController.js
+++ controller/esameController.js
@@ -1,73 +1,75 @@
-const {Pool} = require('pg')
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Unipg',
-  password: 'postgres',
-})
+	user: 'postgres',
+	host: 'localhost',
+	database: 'Unipg',
+	password: 'postgres'
+});
 
 // usato per accettare un esame
 const accettaEsame = (request, response) => {
-  const {matricola, corso} = request.body
-  pool.query(
-    "UPDATE esame SET statoesame = 'Accettato' WHERE matricola = $1 and id_corso = $2",
-    [matricola, corso],
-    (error) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User modified: ${matricola}`)
-    }
-  )
-}
+	const { matricola, corso } = request.body;
+	pool.query(
+		"UPDATE esame SET statoesame = 'Accettato' WHERE matricola = $1 and id_corso = $2",
+		[ matricola, corso ],
+		(error) => {
+			if (error) {
+				throw error;
+			}
+			response.status(200).send(`User modified: ${matricola}`);
+		}
+	);
+};
 
 //usato per rifiutare e quindi eliminare un esame
 const rifiutaEsame = (request, response) => {
-  const {matricola,corso} = request.params
-  pool.query('DELETE FROM esame WHERE matricola = $1 AND id_corso = $2', 
-  [matricola,corso], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`Esame deleted: ${matricola}, ${corso}`)
-  })
-}
+	const { matricola, corso } = request.params;
+	pool.query('DELETE FROM esame WHERE matricola = $1 AND id_corso = $2', [ matricola, corso ], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		response.status(200).send(`Esame deleted: ${matricola}, ${corso}`);
+	});
+};
 
 // usato per ricevere la lista degli esami di un certo studente
-const getStudEsami = (request, response) => { 
-  const matricola = (request.params.matricola)
-  pool.query('SELECT corso.id_corso, corso.nome as esame, professore.nome,professore.cognome, esame.dataesame, esame.voto, esame.statoesame FROM esame JOIN corso ON esame.id_corso = corso.id_corso JOIN professore on corso.professore = professore.id AND esame.matricola = $1',
-   [matricola], 
-  (error, results) => {
-    if (error) {
-      throw error
-    }
-    if(results.rows.length > 0){
-        return response.status(200).json(results.rows)
-    }else {
-      return {
-      }
-    }
-    
-  })
-}
+const getStudEsami = (request, response) => {
+	const matricola = request.params.matricola;
+	pool.query(
+		'SELECT corso.id_corso, corso.nome as esame, professore.nome,professore.cognome, esame.dataesame, esame.voto, esame.statoesame FROM esame JOIN corso ON esame.id_corso = corso.id_corso JOIN professore on corso.professore = professore.id AND esame.matricola = $1',
+		[ matricola ],
+		(error, results) => {
+			if (error) {
+				throw error;
+			}
+			if (results.rows.length > 0) {
+				return response.status(200).json(results.rows);
+			} else {
+				return {};
+			}
+		}
+	);
+};
 
 // put di un nuovo voto da parte di un professore
-const inserisciVoto = (request, response) =>{
-  const {corso, matricola, voto, giorno} = request.body
-  pool.query("INSERT INTO esame VALUES ( $1 , $2 , $3 , $4 ,'Waiting')",[corso,matricola,voto,giorno],
-  (error, results) => {
-    if (error){
-      throw error
-    }
-    response.status(201).send(`Esame inserito: ${matricola}, ${corso}`)
-  })
-}
+const inserisciVoto = (request, response) => {
+	const { corso, matricola, voto, giorno } = request.body;
+	pool.query(
+		"INSERT INTO esame VALUES ( $1 , $2 , $3 , $4 ,'Waiting')",
+		[ corso, matricola, voto, giorno ],
+		(error, results) => {
+			if (error) {
+				throw error;
+			}
+			response.status(201).send(`Esame inserito: ${matricola}, ${corso}`);
+		}
+	);
+};
 
 module.exports = {
-  accettaEsame,
-  getStudEsami,
-  rifiutaEsame,
-  inserisciVoto
-}
+	accettaEsame,
+	getStudEsami,
+	rifiutaEsame,
+	inserisciVoto
+};
--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const professore = require ('./controller/professoreController')
-const studente = require ('./controller/studenteController')
-const corso = require ('./controller/corsoController')
-const esame = require ('./controller/esameController')
-const cors = require('cors')
-const app = express()
-const port = 3001
+const express = require('express');
+const bodyParser = require('body-parser');
+const professore = require('./controller/professoreController');
+const studente = require('./controller/studenteController');
+const corso = require('./controller/corsoController');
+const esame = require('./controller/esameController');
+const cors = require('cors');
+const app = express();
+const port = 3001;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-  })
+	response.json({ info: 'Node.js, Express, and Postgres API' });
+});
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+	console.log(`App running on port ${port}.`);
+});
 
-app.post('/professori/login', professore.postProfLogin)
+app.post('/professori/login', professore.postProfLogin);
 
-app.get('/studenti', studente.getStudente)
-app.post('/studenti/email', studente.checkEmail) 
-app.post('/studenti/login', studente.postStudLogin)
-app.post('/studenti/nuovo', studente.postNuovoStudente) 
+app.get('/studenti', studente.getStudente);
+app.post('/studenti/email', studente.checkEmail);
+app.post('/studenti/login', studente.postStudLogin);
+app.post('/studenti/nuovo', studente.postNuovoStudente);
 
-app.get('/corsi/:professore', corso.getCorsoFromProf)
+app.get('/corsi/:professore', corso.getCorsoFromProf);
 
-app.get('/studenti/esami/:matricola',esame.getStudEsami)
-app.post('/esami/nuovo', esame.inserisciVoto)
-app.put('/studenti/esami/', esame.accettaEsame)
-app.delete('/studenti/esami/:corso/:matricola', esame.rifiutaEsame) 
+app.get('/studenti/esami/:matricola', esame.getStudEsami);
+app.post('/esami/nuovo', esame.inserisciVoto);
+app.put('/studenti/esami/', esame.accettaEsame);
+app.delete('/studenti/esami/:corso/:matricola', esame.rifiutaEsame);
