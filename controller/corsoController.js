2c526f2970c4476395b2410b7f5cbda5f10a9df3
FabioBocchini <46478546+FabioBocchini@users.noreply.github.com>
2019-07-15 15:33:37 +0200

    eliminato codice superfluo

--- controller/corsoController.js
+++ controller/corsoController.js
@@ -1,34 +1,24 @@
-const {Pool} = require('pg')
+const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Unipg',
-  password: 'postgres',
+	user: 'postgres',
+	host: 'localhost',
+	database: 'Unipg',
+	password: 'postgres'
 })
 
-const getCorso = (request, response) => {
-  pool.query('SELECT * FROM corso', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+//utilizzato per ricevere i corsi di un certo prof
+const getCorsoFromProf = (request, response) => {
+	const professore = request.params.professore
+
+	pool.query('SELECT id_corso, nome FROM corso WHERE professore = $1', [ professore ], (error, results) => {
+		if (error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
 }
 
-//utilizzato per ricevere i corsi di un certo prof
-const getCorsoFromProf = (request, response) => { 
-  const professore = (request.params.professore)
-
-  pool.query('SELECT id_corso, nome FROM corso WHERE professore = $1', [professore], 
-  (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-})}
-
 module.exports = {
-  getCorso,
-  getCorsoFromProf
+	getCorsoFromProf
 }
--- controller/professoreController.js
+++ controller/professoreController.js
@@ -1,65 +1,68 @@
-const {Pool} = require('pg')
+const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Unipg',
-  password: 'postgres',
+	user: 'postgres',
+	host: 'localhost',
+	database: 'Unipg',
+	password: 'postgres'
 })
 
 const getProfessore = (request, response) => {
-  pool.query('SELECT * FROM professore', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+	pool.query('SELECT * FROM professore', (error, results) => {
+		if (error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
 }
 
-const getProfByEmail = (request, response) => { 
-  const email = (request.params.email)
+const getProfByEmail = (request, response) => {
+	const email = request.params.email
 
-  pool.query('SELECT * FROM professore WHERE email = LOWER($1)', [email], 
-  (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-})}
+	pool.query('SELECT * FROM professore WHERE email = LOWER($1)', [ email ], (error, results) => {
+		if (error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
+}
 
 const postProf = (request, response) => {
-  const {id, nome, cognome, email, password} = request.body
+	const { id, nome, cognome, email, password } = request.body
 
-  pool.query('INSERT INTO professore VALUES ($1, $2, $3, $4, $5)', [id, nome, cognome, email, password], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`Professore added with ID: ${result.insertId}`)
-  })
+	pool.query(
+		'INSERT INTO professore VALUES ($1, $2, $3, $4, $5)',
+		[ id, nome, cognome, email, password ],
+		(error, results) => {
+			if (error) {
+				throw error
+			}
+			response.status(201).send(`Professore added with ID: ${result.insertId}`)
+		}
+	)
 }
 
 //usato per login professore
-const postProfLogin = (request, response) => { 
-  const {email,password}= request.body
-  pool.query('SELECT id, nome, cognome, password FROM professore WHERE email = $1', [email], 
-  (error, results) => {
-    if (error) {
-      throw error
-    }
-    if(results.rows.length > 0){
-      const user = results.rows[0]
-      if(user.password === password) {
-        delete user.password
-        return response.status(200).json(user)
-      }
-    }
-    response.sendStatus(401)
-  })
+const postProfLogin = (request, response) => {
+	const { email, password } = request.body
+	pool.query('SELECT id, nome, cognome, password FROM professore WHERE email = $1', [ email ], (error, results) => {
+		if (error) {
+			throw error
+		}
+		if (results.rows.length > 0) {
+			const user = results.rows[0]
+			if (user.password === password) {
+				delete user.password
+				return response.status(200).json(user)
+			}
+		}
+		response.sendStatus(401)
+	})
 }
 
 module.exports = {
-  getProfessore,
-  getProfByEmail,
-  postProfLogin,
-  postProf
+	getProfessore,
+	getProfByEmail,
+	postProfLogin,
+	postProf
 }
